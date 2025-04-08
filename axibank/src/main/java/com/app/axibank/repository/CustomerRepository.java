42a22ae59a58cdbf3009cb65d52688b227b2fbb6
fkauanGIT <kauanuniversity@gmail.com>
2025-04-08 11:30:47 -0300

    feat: I created the customer, balance, transfer, and transaction repositorys. first model.

---
+++ axibank/src/main/java/com/app/axibank/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.app.axibank.repository;
+
+import com.app.axibank.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ axibank/src/main/java/com/app/axibank/repository/TransferRepository.java
@@ -0,0 +1,9 @@
+package com.app.axibank.repository;
+
+import com.app.axibank.model.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
