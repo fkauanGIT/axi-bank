4f2188e449d1a6ba8f9965f8ac5fad2dcc4eb5e5
fkauanGIT <kauanuniversity@gmail.com>
2025-04-08 10:27:27 -0300

    feat: initializer project

---
+++ axibank/src/main/java/com/app/axibank/AxibankApplication.java
@@ -0,0 +1,13 @@
+package com.app.axibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxibankApplication.class, args);
+	}
+
+}
---
+++ axibank/src/test/java/com/app/axibank/AxibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.axibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
