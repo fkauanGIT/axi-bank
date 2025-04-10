516ac646a96e6abf469eb928e5081f385ec01ae4
fkauanGIT <kauanuniversity@gmail.com>
2025-04-10 13:18:40 -0300

    feat: creating exceptions file

---
+++ axibank/src/main/java/com/app/axibank/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.app.axibank.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAll(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro interno: " + ex.getMessage());
+    }
+}
