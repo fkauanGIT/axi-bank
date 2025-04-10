c1599324eb4dce30e9cacd9aeac7972180b48a69
fkauanGIT <kauanuniversity@gmail.com>
2025-04-10 13:18:20 -0300

    feat: creating LoginRequestDTO and LoginResponseDTO files

---
+++ axibank/src/main/java/com/app/axibank/model/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.app.axibank.model;
+
+public record LoginRequestDTO (String email, String password) {
+}
---
+++ axibank/src/main/java/com/app/axibank/model/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.app.axibank.model;
+
+public record LoginResponseDTO(String name, String jwtToken) {
+}
