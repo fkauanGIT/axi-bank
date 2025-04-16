fb7b061a4d502ff2edaca5e144c1287a5f961609
fkauanGIT <kauanuniversity@gmail.com>
2025-04-16 11:39:47 -0300

    feat: first commit

---
+++ axibank/src/test/java/com/project/axibank/AxibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.axibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
