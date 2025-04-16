fb8117baae852caaa0f0b1f6f14ac8e0e45b30df
fkauanGIT <kauanuniversity@gmail.com>
2025-04-16 12:55:15 -0300

    feat: Creating our own custom tables for Authentication

--- axibank/src/main/resources/sql/scripts.sql
+++ axibank/src/main/resources/sql/scripts.sql
@@ -7,3 +7,14 @@ INSERT IGNORE INTO `authorities` VALUES ('user', 'read');
 
 INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$XsZkW5xSOj6S4iQDQmYV/uZjdJf869qUsWDS9lUw/Od8OSzynGevy', '1'); --AdminName@54321--
 INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{noop}UserName@12345', 'read');
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$XsZkW5xSOj6S4iQDQmYV/uZjdJf869qUsWDS9lUw/Od8OSzynGevy', 'admin');
