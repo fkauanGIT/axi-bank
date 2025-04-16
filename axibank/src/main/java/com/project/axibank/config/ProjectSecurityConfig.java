b36ff0c971efeea151f7e3222689ddbe90035bad
fkauanGIT <kauanuniversity@gmail.com>
2025-04-16 11:57:43 -0300

    feat: Configuring PasswordEncoder using PasswordEncoderFactories and Demo of CompromisedPasswordChecker

--- axibank/src/main/java/com/project/axibank/config/ProjectSecurityConfig.java
+++ axibank/src/main/java/com/project/axibank/config/ProjectSecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -23,6 +27,13 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}UserName@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$XsZkW5xSOj6S4iQDQmYV/uZjdJf869qUsWDS9lUw/Od8OSzynGevy").authorities("admin").build(); //AdminName@12345
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
