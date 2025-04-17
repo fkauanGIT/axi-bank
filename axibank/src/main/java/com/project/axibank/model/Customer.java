5428a004c3505ce7f986eaae5de2f0796377789c
fkauanGIT <kauanuniversity@gmail.com>
2025-04-17 17:49:11 -0300

    feat: Making backend changes to load authorities from new DB table

--- axibank/src/main/java/com/project/axibank/model/Customer.java
+++ axibank/src/main/java/com/project/axibank/model/Customer.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -33,4 +34,7 @@ public class Customer {
     @JsonIgnore
     private Date createDt;
 
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Authority> authorities;
 }
