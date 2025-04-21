373497a092a2870a0b4491ee831041bb844ac809
fkauanGIT <kauanuniversity@gmail.com>
2025-04-21 13:39:18 -0300

    feat: Demo of @PostFilter annotation

--- axibank/src/main/java/com/project/axibank/controller/ContactController.java
+++ axibank/src/main/java/com/project/axibank/controller/ContactController.java
@@ -3,11 +3,14 @@ package com.project.axibank.controller;
 import com.project.axibank.model.Contact;
 import com.project.axibank.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -17,10 +20,17 @@ public class ContactController {
     private final ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
-        contact.setContactId(getServiceReqNumber());
-        contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        List<Contact> returnContacts = new ArrayList<>();
+        if(!contacts.isEmpty()) {
+            Contact contact = contacts.getFirst();
+            contact.setContactId(getServiceReqNumber());
+            contact.setCreateDt(new Date(System.currentTimeMillis()));
+            Contact savedContact = contactRepository.save(contact);
+            returnContacts.add(savedContact);
+        }
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
