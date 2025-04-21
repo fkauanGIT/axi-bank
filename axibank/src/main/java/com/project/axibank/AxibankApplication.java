1769b211463476ec5a3ee4bbcdfa5f8252c0daba
fkauanGIT <kauanuniversity@gmail.com>
2025-04-21 13:06:34 -0300

    feat: Introduction to method level security in Spring Security

--- axibank/src/main/java/com/project/axibank/AxibankApplication.java
+++ axibank/src/main/java/com/project/axibank/AxibankApplication.java
@@ -2,8 +2,10 @@ package com.project.axibank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true, jsr250Enabled = true, securedEnabled = true)
 public class AxibankApplication {
 
 	public static void main(String[] args) {
